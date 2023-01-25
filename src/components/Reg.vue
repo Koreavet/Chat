6a2c64fd2a4ada6993769c078926a7afd1aa2828
Giranda22-Git <61464174+Giranda22-Git@users.noreply.github.com>
2023-01-25 18:38:37 +0600

    init project

---
+++ src/components/Reg.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <input type="text" placeholder="Login" class="login">
+    <input type="text" placeholder="Password" class="password">
+    <input type="text" placeholder="try password" class="tryPassword">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Reg'
+}
+</script>
+
+<style lang="less" scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    border: 1px solid gray;
+    border-radius: 8px;
+    padding: 15px;
+    input {
+      width: 80%;
+      padding: 8px 16px;
+      outline: none;
+      border: 1px solid rgba(140, 145, 150, .6);
+      border-radius: 3px;
+      &:hover {
+        border: 1px solid rgba(140, 145, 150, 1);
+      }
+    }
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Auth id="auth" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Auth from '@/components/Auth.vue'
+import Reg from '@/components/Reg.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Auth,
+    Reg
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  #auth {
+    width: 40%;
+    height: 25%;
+  }
+}
+  
+</style>
